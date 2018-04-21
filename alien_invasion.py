2ba5cf8bcea3fecaee5eca0a5736c2b0679caecd
qetennyson <quincytennyson@yahoo.com>
2018-04-21 18:48:09 -0400

    Refactored event checking and screen drawing to game_functions.py Key presses should be tracked, but no response on MBA.
    Ship has two new boolean attributes of moving_right/left based on tracked events.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,6 @@
-import sys
-
 import pygame
 
+import game_functions as gf
 from settings import Settings
 from ship import Ship
 
@@ -21,17 +20,9 @@ def run_game():
     # Start the main game loop.
     while True:
 
-        # Watch for keyboard/mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
