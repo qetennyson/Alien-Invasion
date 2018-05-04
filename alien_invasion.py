5eaa2a9259e1fe54266196e031c1a0163e7ee5c8
Quincy Tennyson <quincytennyson8@gmail.com>
2018-05-04 10:25:22 -0400

    * added moving alien fleet * added collisions * added stat tracking for ship * added play button and functionality

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,9 @@ import pygame
 import game_functions as gf
 from settings import Settings
 from pygame.sprite import Group
+from game_stats import GameStats
+from button import Button
+
 
 from ship import Ship
 
@@ -16,6 +19,12 @@ def run_game():
 
     pygame.display.set_caption("Alien Invasion")
 
+    # Make the Play button.
+    play_button = Button(ai_settings, screen, "Play")
+
+    # Create an instance to store game statistics.
+    stats = GameStats(ai_settings)
+
     # Make a ship, a group of aliens, and a group of bullets.
 
     ship = Ship(ai_settings, screen)
@@ -28,10 +37,15 @@ def run_game():
     # Start the main game loop.
     while True:
 
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,8 +1,24 @@
 import sys
 
 import pygame
+from time import sleep
 from bullet import Bullet
 from alien import Alien
+
+
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
+    """Respond to keypresses and mouse events"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(ai_settings, screen, stats, play_button, ship,
+                              aliens, bullets, mouse_x, mouse_y)
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
@@ -17,24 +33,6 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         sys.exit()
 
 
-def fire_bullet(ai_settings, screen, ship, bullets):
-    """ Fire a bullet if the limit is not yet reached."""
-    if len(bullets) < ai_settings.bullets_allowed:
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
-
-
-def check_events(ai_settings, screen, ship, bullets):
-    """Respond to keypresses and mouse events"""
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ai_settings, screen, ship, bullets)
-        elif event.type == pygame.KEYUP:
-            check_keyup_events(event, ship)
-
-
 def check_keyup_events(event, ship):
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
@@ -42,7 +40,24 @@ def check_keyup_events(event, ship):
                 ship.moving_left = False
 
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
+                      bullets, mouse_x, mouse_y):
+        """Start a new game when the player clicks Play."""
+        if play_button.rect.collidepoint(mouse_x, mouse_y):
+            # Reset the game stats.
+            stats.reset_stats()
+            stats.game_active = True
+
+            # Emtpy the list of aliens and bullets
+            bullets.empty()
+            aliens.empty()
+
+            # Create a new fleet and center the ship
+            create_fleet(ai_settings, screen, ship, aliens)
+            ship.center_ship()
+
+
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen"""
 
     # Redraw the screen during each pass through the loop
@@ -54,11 +69,22 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
 
+    # Draw the play button if the game is inactive.
+    if not stats.game_active:
+        play_button.draw_button()
+
     # Make the most recently drawn screen visible.
     pygame.display.flip()
 
 
-def update_bullets(bullets):
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """ Fire a bullet if the limit is not yet reached."""
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """ Update position of bullets and get rid of old bullets """
     # Update bullet positions.
     bullets.update()
@@ -68,6 +94,19 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
     print(len(bullets))
+
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """Respond to any bullet-alien collisions"""
+    # Remove any bullets and aliens that have collided.
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if len(aliens) == 0:
+        # Destroy existing bullets and create new fleet.
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
 
 
 def get_number_aliens_x(ai_settings, alien_width):
@@ -110,4 +149,67 @@ def create_fleet(ai_settings, screen, ship, aliens):
             # Create an alien and place it in the row
             create_alien(ai_settings, screen, aliens, alien_number,
                          row_number)
+
+
+def change_fleet_direction(ai_settings, aliens):
+    """Drop the entire fleet and change the fleet's direction"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+
+def check_fleet_edges(ai_settings, aliens):
+    """Respond appropriately if any aliens have reached an age."""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+    """Check if the fleet is at an edge,
+        and then update the position of all aliens in the fleet."""
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
+    # Look for alien-ship collisions.
+    if pygame.sprite.spritecollideany(ship, aliens):
+        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+        print("Ship hit!!! OMG")
+
+    # Look for aliens hitting the bottom of the screen.
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+
+
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+    """Respond to ship being hit by alien"""
+    # Decrement ships left.
+    if stats.ships_left > 0:
+        stats.ships_left -= 1
+
+        # Empty the list of aliens and bullets.
+        aliens.empty()
+        bullets.empty()
+
+        # Create a new fleet and center the ship.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+
+        # Pause.
+        sleep(0.5)
+
+    else:
+        stats.game_active = False
+
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """Check if any aliens have reached the bottom of the screen."""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # Treat this the same as if the ship got hit.
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
+
+
 
