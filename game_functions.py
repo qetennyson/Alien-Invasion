2826ac778e511bf862c21cedab5c64f494d74aed
Quincy Tennyson <quincytennyson8@gmail.com>
2018-05-04 07:13:38 -0400

    adding bullets

--- game_functions.py
+++ game_functions.py
@@ -8,14 +8,20 @@ def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+
+def check_keydown_events(event, ship):
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
             elif event.key == pygame.K_LEFT:
                 ship.moving_left = True
 
-        elif event.type == pygame.KEYUP:
+
+def check_keyup_events(event, ship):
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
             elif event.key == pygame.K_LEFT:
